7923d1033437c5580cde82f7351d217abe45070c
Brad Babines <bbabines@gmail.com>
2023-05-19 13:48:36 -0400

    feat: Added styles for the FormComponents demo page. The + and - sign do not functio at this time. Will add state once team checks off on the styles

--- app/FormComponents/page.tsx
+++ app/FormComponents/page.tsx
@@ -2,7 +2,7 @@ const FormComponents = () => {
 	return (
 		<div className="bg-white">
 			<div className="m-4">
-				<label htmlFor="fname" className="text-[12px] text-black">
+				<label htmlFor="fname" className="text-[12px] text-black ">
 					Name
 				</label>
 				<br />
@@ -10,7 +10,7 @@ const FormComponents = () => {
 			</div>
 
 			<div className="m-4">
-				<label htmlFor="password" className="text-[12px] text-black">
+				<label htmlFor="password" className="text-[12px] text-black ">
 					Password
 				</label>
 				<br />
@@ -19,7 +19,8 @@ const FormComponents = () => {
 
 			<div className="m-4 border border-[#cfcfcf;] w-[309px] h-[56px] flex  items-center hover:border-[#554ad8]">
 				<input
-					className="h-[20px] w-[20px] border border-[#cfcfcf;] mr-[16px] ml-[16px] checked:bg-purple-500"
+					className=" border border-[#cfcfcf;] mr-[16px] ml-[16px] checked:bg-[#554AD8] rounded-full 
+					checked:h-[25px] checked:rounded-full"
 					type="radio"
 				/>
 				<label className="text-[14px]" htmlFor="password">
@@ -27,10 +28,19 @@ const FormComponents = () => {
 				</label>
 			</div>
 
-			<div className="m-4">
+			<div className="m-4 ">
 				<label>Number</label>
 				<br />
-				<input type="number" />
+				<button className="p-[2px]  ml-4 transform translate-y-[40px] hover:text-[#de9a74]">
+					-
+				</button>
+				<button className="p-[2px]  ml-6 transform translate-y-[40px] translate-x-[40px] hover:text-[#de9a74]">
+					+
+				</button>
+				<input
+					className="appearance-none block bg-[#f1f1f1] h-[48px] w-[120px] text-center"
+					type="number"
+				/>
 			</div>
 
 			<div className="m-4">
