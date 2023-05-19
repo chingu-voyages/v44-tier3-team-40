f45861f6009d3db5b16178ec02bbdd7ccdd6eaf3
unknown <bbabines@gmail.com>
2023-05-19 01:34:48 -0400

    feat: Added styles to demo form components. Next.js/Tailwind default focus style needs removed for new focus style to take effect. Additionally, the background of the check radio button does not seem to be taken effect

--- app/FormComponents/page.tsx
+++ app/FormComponents/page.tsx
@@ -1,34 +1,44 @@
 const FormComponents = () => {
 	return (
-		<>
+		<div className="bg-white">
 			<div className="m-4">
-				<label htmlFor="fname" className="text-sm">
+				<label htmlFor="fname" className="text-[12px] text-black">
 					Name
 				</label>
 				<br />
-				<input type="text" />
+				<input type="text" placeholder="Insert your name" />
 			</div>
 
 			<div className="m-4">
-				<label htmlFor="password">Password</label>
-				<input type="text" placeholder="Wrong Format" />
+				<label htmlFor="password" className="text-[12px] text-black">
+					Password
+				</label>
+				<br />
+				<input type="password" placeholder="Wrong Format Example" />
 			</div>
 
-			<div className="m-4">
-				<label htmlFor="password">Radio Button</label>
-				<input type="radio" />
+			<div className="m-4 border border-[#cfcfcf;] w-[309px] h-[56px] flex  items-center hover:border-[#554ad8]">
+				<input
+					className="h-[20px] w-[20px] border border-[#cfcfcf;] mr-[16px] ml-[16px] checked:bg-purple-500"
+					type="radio"
+				/>
+				<label className="text-[14px]" htmlFor="password">
+					Patient 1
+				</label>
 			</div>
 
 			<div className="m-4">
 				<label>Number</label>
+				<br />
 				<input type="number" />
 			</div>
 
 			<div className="m-4">
-				<label>Number</label>
-				<input type="textarea" />
+				<label>Reason for visit</label>
+				<br />
+				<textarea placeholder="Type your message here" />
 			</div>
-		</>
+		</div>
 	);
 };
 
