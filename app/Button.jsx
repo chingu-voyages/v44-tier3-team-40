806f3033e66fa82d3f28e665d1a75730b1ca1b63
Khemmie-Ray02 <atokemmy@gmail.com>
2023-05-20 23:17:36 +0100

    add custom buttons

---
+++ app/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Button() {
+  return (
+    <div>
+      <button className="uppercase font-[700] text-white text-[13px] hover:bg-[#a4a4ff] bg-[#554ad8] w-[160px] h-[48px]">See product</button>
+    </div>
+  )
+}
