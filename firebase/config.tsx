44aecb01f1e556542ab71ff8deef229dc65df4d3
Kathleen Mercado <kathleenmerc@gmail.com>
2023-05-24 16:42:18 -0400

    feat: set up firebase real time database

--- firebase/config.tsx
+++ firebase/config.tsx
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
+import { getDatabase } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -13,7 +14,8 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
 };
 
 
@@ -21,5 +23,6 @@ const firebaseConfig = {
 // const firebase_app = initializeApp(firebaseConfig);
 let firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 // const analytics = getAnalytics(firebase_app);
+export const database = getDatabase(firebase_app);
 
 export default firebase_app;
