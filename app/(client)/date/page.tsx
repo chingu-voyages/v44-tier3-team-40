953c5e7aa00600abfb130b39a76a3f2a3f9f8fb5
Sean Redmon <seanredmon@seanred.io>
2023-05-24 15:57:47 -0400

    wip

--- app/(client)/date/page.tsx
+++ app/(client)/date/page.tsx
@@ -1,17 +1,29 @@
+"use client";
+import Calendar from "react-calendar";
 import ProgressStepper from "@/app/components/ProgressStepper";
-import { FC } from "react";
+import { FC, useState } from "react";
+import "./Calendar.css";
 
 interface ChooseADatePageProps {}
 
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 const ChooseADatePage: FC<ChooseADatePageProps> = () => {
+  const [value, onChange] = useState<Value>(new Date());
+
   return (
     <div>
       <h1 className="text-[40px]">Western Rehab PT</h1>
-      <ProgressStepper
-        currentStep={3}
-        length={5}
-        label="Select a date and time slot"
-      />
+      <div className="w-[184px]">
+        <ProgressStepper
+          currentStep={3}
+          length={5}
+          label="Select a date and time slot"
+        />
+      </div>
+      <Calendar onChange={onChange} value={value} prevLabel="<" nextLabel=">" />
     </div>
   );
 };
