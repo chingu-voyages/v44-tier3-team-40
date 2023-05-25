e8a38a37fe8b1dce2669c34d17d243dbc021b2c8
Khemmie-Ray02 <atokemmy@gmail.com>
2023-05-25 20:55:23 +0100

    updates to custom buttons

--- app/ButtonTwo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ButtonTwo() {
-  return (
-    <div>
-      <button className="uppercase font-[700] text-black text-[13px] hover:bg-black hover:text-white bg-white w-[160px] h-[48px]">See product</button>
-    </div>
-  )
-}
